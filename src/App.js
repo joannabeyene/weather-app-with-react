4cbc120ba6f932c4338ee17cc265add95000026d
Joanna Beyene <joanna.beyene@gmail.com>
2021-09-29 14:59:04 +0200

    created a weather component in a separate file

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
-import React, {useState} from 'react';
-import DateBuilder from './components/DateBuilder';
-
-const api = {
-  key: 'ca419d860870597f10881db6e7521d4c',
-  base: 'http://api.openweathermap.org/data/2.5/'
-}
+import React from 'react';
+import Weather from './components/Weather'
 function App() {
-
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState('');
-
-  const search = evt => {
-    if (evt.key === 'Enter') {
-        fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-        setWeather(result);
-        setQuery('');
-        console.log(result)
-        }).catch(err => {
-            console.log('Error Reading data: ' + err);
-        });
-    }
-  }
-
   return (
     <div className='App'>
-     <main>
-       <div className='search-box'>
-        <input type="text" 
-        className='search-bar' 
-        placeholder='Search...'
-        onChange={e => setQuery(e.target.value)}
-        value={query}
-        onKeyPress={search}/>
-        </div>
-        {(typeof weather.main != 'undefined') ? (
-          <div>
-            <div className='location-box'>
-              <div className='location'>{weather.name},{weather.sys.country}</div>
-              <div className='date'><DateBuilder /></div>
-            </div>
-            <div className='weather-box'>
-              <div className='temp'>
-                {Math.round(weather.main.temp)}ºc
-              </div>
-              <div className='weather'>
-                {weather.weather[0].description}
-              </div>
-            </div>
-          </div>
-        ) : ('')}
-     </main>
+     <Weather/>
     </div>
   );
 }
